d5925694be163b4e8a2aae07d9b936ad2cbacdab
Sanjana <sanjanaasingh6@gmail.com>
2025-06-26 01:02:12 +0530

    Add Vendor schema model

---
+++ src/lib/models/vendor.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const VendorSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  bankAccountNo: { type: String, required: true },
+  bankName: { type: String, required: true },
+  addressLine1: String,
+  addressLine2: String,
+  city: String,
+  country: String,
+  zipCode: String,
+}, {
+  timestamps: true,
+});
+
+export default mongoose.models.Vendor || mongoose.model('Vendor', VendorSchema);
